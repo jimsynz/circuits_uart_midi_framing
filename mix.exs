76d3e0c750bf95269b3615bf21994c4e0d18b9c2
James Harton <james@harton.nz>
2021-12-09 13:24:41 +1300

    chore: run Elixir formatter.

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule Circuits.UART.Framing.MIDI.MixProject do
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:earmark, ">= 0.0.0", only: [:dev, :test]},
       {:ex_doc, ">= 0.0.0", only: [:dev, :test]},
-      {:git_ops, "~> 2.3", only: ~w[dev test]a, runtime: false},
+      {:git_ops, "~> 2.3", only: ~w[dev test]a, runtime: false}
     ]
   end
 end
